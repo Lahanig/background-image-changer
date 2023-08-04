232612d3d73854b106e74720eacd2d2b356ea7b4
Kaibi <40522793+Lahanig@users.noreply.github.com>
2023-08-04 14:00:23 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let background = localStorage.getItem("background")
+
+if (!background) {
+    localStorage.setItem("background", JSON.stringify(null))
+}
+
+class Storage {
+    constructor() {
+        this.backgroundUrl = JSON.parse(localStorage.getItem("background"))
+    }
+
+    setBackground() {
+        let body = document.querySelector("body")
+
+        body.style.background = `url("${this.backgroundUrl}") no-repeat center`
+        body.style.backgroundSize = "cover"
+    }
+
+    updateBackground() {
+        let body = document.querySelector("body"),
+            url = document.querySelector("#urlInput").value
+
+        body.style.background = `url("${url}") no-repeat center`
+        body.style.backgroundSize = "cover"
+
+        localStorage.setItem("background", JSON.stringify(url))
+
+        document.querySelector("#urlInput").value = ""
+    }
+}
+
+const Store = new Storage()
+
+window.onload = () => {
+    Store.setBackground()
+}
