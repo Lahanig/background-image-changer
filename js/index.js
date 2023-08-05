2eed8ed35a776e52cf46b0e6ac4d22b84cbdec96
Kaibi <40522793+Lahanig@users.noreply.github.com>
2023-08-05 16:00:08 +0300

    Rework upload file method

--- js/index.js
+++ js/index.js
@@ -1,33 +1,45 @@
 let background = localStorage.getItem("background")
 
 if (!background) {
-    localStorage.setItem("background", JSON.stringify(null))
+    localStorage.setItem("background", "null")
 }
 
 class Storage {
     constructor() {
-        this.backgroundUrl = JSON.parse(localStorage.getItem("background"))
+        this.backgroundUrl = localStorage.getItem("background")
     }
 
     setBackground() {
-        if (this.backgroundUrl === null) return
+        if (this.backgroundUrl === "null") return
 
-        let body = document.querySelector("body")
+        let body = document.querySelector("body"),
+            imageUrl = localStorage.getItem('background')
 
-        body.style.background = `url("${this.backgroundUrl}") no-repeat center`
+        body.style.background = `url(data:image/png;base64,${imageUrl}) no-repeat center`
         body.style.backgroundSize = "cover"
     }
 
     updateBackground() {
         let body = document.querySelector("body"),
-            url = document.querySelector("#urlInput").value
+            url = document.querySelector("#urlInput")
 
-        body.style.background = `url("${url}") no-repeat center`
+        url.addEventListener('change', (e) => {
+            const file = e.target.files[0],
+                 reader = new FileReader()
+            reader.onloadend = () => {
+                // convert file to base64 String
+                const base64String = reader.result.replace('data:', '').replace(/^.+,/, '')
+                // store file
+                localStorage.setItem('background', base64String)
+            }
+            reader.readAsDataURL(file)
+        })
+
+        let imageUrl = localStorage.getItem('background')
+
+        // display image
+        body.style.background = `url(data:image/png;base64,${imageUrl}) no-repeat center`
         body.style.backgroundSize = "cover"
-
-        localStorage.setItem("background", JSON.stringify(url))
-
-        document.querySelector("#urlInput").value = ""
     }
 }
 
